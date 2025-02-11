bb5a2f905960ef3ed0b50bad0a125deb4a69b33c
Ansh tyagi <anshtyagi7845@gmail.com>
2025-02-11 15:44:43 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,16 +27,22 @@ export default function DashboardPage() {
     }, []);
     return (
         <div className="space-y-6 p-6 md:p-10 max-w-screen-xl mx-auto overflow-hidden overflow-x-hidden text-start">
-            <div className="flex justify-end m-5 p-4 relative text-end text-white">
-                <input
-                    id="datepicker-button"
-                    type="text"
-                    readOnly
-                    className="btn btn-primary "
-                    placeholder="Select Date Range"
-                    value={selectedDate}
-                />
+             <div className="flex flex-col items-center space-y-8">
+            <h2 className="text-3xl font-bold tracking-tight mt-3 mb-1 text-center">
+                Dashboard & Reporting Overview
+            </h2>
             </div>
+            {/* <div className="flex justify-end mt-5 mb-2 p-4 relative text-end text-white">
+            <input
+                id="datepicker-button"
+                type="text"
+                readOnly
+                className="btn btn-primary "
+                placeholder="Select Date Range"
+                value={selectedDate}
+            />
+              
+            </div> */}
 
             <div>
                 <BalanceBoxes />
